fb9de3b70f55273e61d0c3d020b6b40d46aa3a79
Andre Kestler <a.kestler@oth-aw.de>
2023-05-12 11:06:47 +0200

    get bucket to edit and close connection to s3 again

--- sys-src/backend/app/connections/aws_s3.py
+++ sys-src/backend/app/connections/aws_s3.py
@@ -5,25 +5,39 @@ import os
 import boto3
 
 def get_s3_connection():
-    # get boto3 session with access_key_id and secret_access_key from ./aws/credentials
-    session = boto3.Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_DEFAULT_REGION'])
-    return session
+    """ Get the connection to AWS and the s3 ressource """
+    # get boto3 session to connect to AWS
+    aws_session = boto3.Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_DEFAULT_REGION'])
+    
+    # get s3 ressource
+    s3_ressource = aws_session.resource('s3')
 
-def get_s3_bucket(session):
+    return s3_ressource
+
+def get_s3_bucket(s3_ressource):
+    """ Get the s3 bucket from aws """
+    str_bucket_name = 'team-rot-fatcat-data'        # bucket name
     # get s3 buckets
-    s3_bucket = session.resource('s3')
+    s3_bucket = s3_ressource.Bucket(str_bucket_name)
 
     return s3_bucket
 
-def get_s3_object():
+def get_s3_object(s3_bucket, data):
     pass
 
-def put_s3_object():
+def put_s3_object(s3_bucket, data):
     pass
+
+def close_s3_connection(s3_ressource):
+    """ Close the connection to AWS and the s3 bucket """
+    s3_ressource.meta.client.close()
 
 
 if __name__ == '__main__':
-    aws_session = get_s3_connection()
-    s3_bucket = get_s3_bucket(aws_session)
+    s3_ressource = get_s3_connection()
+    s3_bucket = get_s3_bucket(s3_ressource)
 
-    print(s3_bucket)
+    for obj in s3_bucket.objects.all():
+        print(obj.key)
+
+    close_s3_connection(s3_ressource)
