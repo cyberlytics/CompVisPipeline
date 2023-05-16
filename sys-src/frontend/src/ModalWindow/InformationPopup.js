d0332eed14c7f4901455ee9910a293fb28d59492
Tobias Lettner <t.lettner@oth-aw.de>
2023-05-16 17:34:28 +0200

    Allgemeines Popupwindow zur Anzeige fuer Informationen implementiert

---
+++ sys-src/frontend/src/ModalWindow/InformationPopup.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+/**
+ * PopupWindow to show some informations
+ * @param  {[boolean]} open         open dialog, if true
+ * @param  {[function]} onClose     function to close the dialog
+ * @param  {[string]} headerText    defines the header text
+ * @param  {[string]} text          defines the information text
+ * @return {[Dialog]}               Dialog
+ */
+export default function InformationPopup({ open, onClose, headerText, text }) {
+    return (
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {headerText}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {text}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    );
+  }
