6e4dc5b055ca6960abd87268d8f346f04738f7da
Andre Kestler <a.kestler@oth-aw.de>
2023-05-22 16:03:39 +0200

    changed loading AWS credentials

--- sys-src/frontend/src/Components/Connections/aws_s3.js
+++ sys-src/frontend/src/Components/Connections/aws_s3.js
@@ -1,31 +1,35 @@
 // Connection for AWS S3
 
-const STR_BUCKET_NAME = 'team-rot-fatcat-data';
-
 // Get AWS SDK
 function getAWSSDK() {
     var AWS = require("aws-sdk");
+
+    // get credentials from env
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+    const region = process.env.AWS_DEFAULT_REGION;
+
+    // set credentials
+    AWS.config.update({
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey,
+        region: region
+    })
 
     AWS.config.getCredentials(function(err) {
       if (err) console.log(err.stack);
       // credentials not loaded
       else {
-
-        console.log("Access key:", AWS.config.credentials.accessKeyId);
-        console.log("Access key:", AWS.config.credentials.secretAccessKey);
-        
-        // set region from env
-        const region = process.env.AWS_DEFAULT_REGION;
-        AWS.config.update({region: region});
-
-        console.log("Access key:", AWS.config.region);
+        console.log("AWS Credentials loaded successfully")
       }
     });
+
+    return AWS;
 
 }
 
 // Get connection to S3
-function getS3Connection() {
+function getS3Connection(AWS, bucketName='team-rot-fatcat-data') {
 
 }
 
