3949971d59d909d80b40d41e89fd1313e2fddf67
Andre Kestler <a.kestler@oth-aw.de>
2023-05-22 15:58:28 +0200

    set AWS connection

--- sys-src/frontend/src/Components/Connections/aws_s3.js
+++ sys-src/frontend/src/Components/Connections/aws_s3.js
@@ -1,1 +1,45 @@
 // Connection for AWS S3
+
+const STR_BUCKET_NAME = 'team-rot-fatcat-data';
+
+// Get AWS SDK
+function getAWSSDK() {
+    var AWS = require("aws-sdk");
+
+    AWS.config.getCredentials(function(err) {
+      if (err) console.log(err.stack);
+      // credentials not loaded
+      else {
+
+        console.log("Access key:", AWS.config.credentials.accessKeyId);
+        console.log("Access key:", AWS.config.credentials.secretAccessKey);
+        
+        // set region from env
+        const region = process.env.AWS_DEFAULT_REGION;
+        AWS.config.update({region: region});
+
+        console.log("Access key:", AWS.config.region);
+      }
+    });
+
+}
+
+// Get connection to S3
+function getS3Connection() {
+
+}
+
+
+// Get image from S3
+function getImageFromS3() {
+
+}
+
+
+// Upload image to S3
+function pushImageToS3() {
+
+}
+
+// Test script
+getAWSSDK();
