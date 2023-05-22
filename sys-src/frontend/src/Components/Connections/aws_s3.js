b9e3d88136bad1cfceb38b8759d1ffc322ce7b40
Andre Kestler <a.kestler@oth-aw.de>
2023-05-22 16:42:08 +0200

    deleteObject() - done

--- sys-src/frontend/src/Components/Connections/aws_s3.js
+++ sys-src/frontend/src/Components/Connections/aws_s3.js
@@ -2,6 +2,25 @@
 // S3.headBucket()
 // headBucket(params = {}, callback) ⇒ AWS.Request
 // This action is useful to determine if a bucket exists and you have permission to access it.
+/*
+Test AWSSDK
+    Check Credentials
+*/
+
+/*
+Test S3Connection
+    const params = { Bucket: bucketName };
+
+    console.log(s3.headBucket(params, function(err, data) {
+        if (err) {
+            console.log("Zugriff auf S3 Bucket fehlgeschlagen");
+        } else {
+            console.log("Zugriff auf S3 Bucket erfolgreich");
+        }
+    }));
+*/
+
+
 
 // Get AWS SDK
 function getAWSSDK() {
@@ -38,22 +57,20 @@ function getS3Connection(AWS, bucketName='team-rot-fatcat-data') {
     // Creat S3 instance
     let s3 = new AWS.S3();
 
-    const params = { Bucket: bucketName };
-
-    console.log(s3.headBucket(params, function(err, data) {
-        if (err) {
-            console.log("Zugriff auf S3 Bucket fehlgeschlagen");
-        } else {
-            console.log("Zugriff auf S3 Bucket erfolgreich");
-        }
-    }));
-
     return s3;
 }
 
 function deleteImageFromS3(S3, bucketName='team-rot-fatcat-data', imageName) {
     // deleteObject()
+    let params = {
+        Bucket: bucketName,
+        Key: imageName
+    }
 
+    S3.deleteObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log("Image deleted from S3");           // successful response
+    });
 }
 
 function deleteAllImagesFromS3(S3, bucketName='team-rot-fatcat-data') {
