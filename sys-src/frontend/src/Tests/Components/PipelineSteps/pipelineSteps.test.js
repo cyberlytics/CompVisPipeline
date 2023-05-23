a00e502a8e039829b14b8d45edf90d9fb97027ea
Tobias Lettner <t.lettner@oth-aw.de>
2023-05-23 21:47:21 +0200

    Searchbar für Pipelinesteps implementiert und Test erweitert
    Former-commit-id: 3bc8693c1adaed935c45a0935d0d4a288e3981aa

--- sys-src/frontend/src/Tests/Components/PipelineSteps/pipelineSteps.test.js
+++ sys-src/frontend/src/Tests/Components/PipelineSteps/pipelineSteps.test.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import PipelineSteps from "../../../Components/PipelineSteps/pipelineSteps";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 describe("pipelineSteps.js tests", () => {
 
+  afterEach(() => {
+    cleanup();
+  });
+
   test("card should be visible", () => {
-    render(<PipelineSteps />);
+    render(<DndProvider backend={HTML5Backend}><PipelineSteps /></DndProvider>);
     const cardVisible = screen.queryByText(/Available Steps/i);
     expect(cardVisible).toBeVisible();
+  });
+
+  test("searchBar should be visible", () => {
+    const pipelineSteps = render(<DndProvider backend={HTML5Backend}><PipelineSteps /></DndProvider>);
+    const textField = pipelineSteps.container.getElementsByClassName("SearchBar-TextField")
+    expect(textField).toBeInTheDocument
   });
 
 });
