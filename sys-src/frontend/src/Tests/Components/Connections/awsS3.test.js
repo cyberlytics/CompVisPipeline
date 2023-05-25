5756eee296faf8c66c08241fc19cabd0dc08bcb2
Andre Kestler <a.kestler@oth-aw.de>
2023-05-25 08:14:27 +0200

    test

--- sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
+++ sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
@@ -9,3 +9,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import S3Manager from "../../../Components/Connections/awsS3";
+
+test("test" , () => {
+    
+});
