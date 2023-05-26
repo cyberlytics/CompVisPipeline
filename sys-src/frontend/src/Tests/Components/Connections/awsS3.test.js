241ee28f47a8efe7e28ad6579b0519bf39d92470
Andre Kestler <a.kestler@oth-aw.de>
2023-05-26 14:24:00 +0200

    deleteImageFromS3() - test done

--- sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
+++ sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
@@ -22,15 +22,24 @@ describe("s3Manager - connection", () => {
         expect(s3Manager.secretAccessKey).toBe(process.env.AWS_SECRET_ACCESS_KEY);
         expect(s3Manager.region).toBe(process.env.AWS_DEFAULT_REGION);
     });
+
+    test("Check S3 Access is successfull", () => {
+        //TODO
+    });
+
+    test("Check S3 Access is denied", () => {
+        //TODO
+    })
 });
 
-describe("s3Manager.deleteImageFromS3()", () => {
-    test("Check delete Image from S3", async () => {
+describe("s3Manager - delete Functions", () => {
+    test("Check S3Manager.deleteImageFromS3", async () => {
         // Mock the deleteImageFromS3 function
         deleteImageFromS3Mock = jest.fn();
         deleteImageFromS3Mock.mockImplementation((imageKey) => {
             return new Promise((resolve, reject) => {
-                resolve("Success");
+                const deleteImage = {};
+                resolve(deleteImage);
             })
         });
 
@@ -38,11 +47,15 @@ describe("s3Manager.deleteImageFromS3()", () => {
         s3Manager.deleteImageFromS3 = deleteImageFromS3Mock;
 
         // Use mock in test
-        const response = await s3Manager.deleteImageFromS3("test_key.jpg")
-        expect(response).toBe("Success");
-        expect(deleteImageFromS3Mock).toHaveBeenCalledTimes(1);
+        s3Manager.deleteImageFromS3("test_key.jpg")
+            // check if promise is resolved
+            .then((result) => {
+                // Check if promise is empty 
+                expect(result).toEqual({});
+                // Check if mock was 1 time called
+                expect(deleteImageFromS3Mock).toHaveBeenCalledTimes(1);
+                // Check if mock was called with correct parameters
+                expect(deleteImageFromS3Mock).toHaveBeenCalledWith("test_key.jpg");
+            });
     });
-
-
-
 });
