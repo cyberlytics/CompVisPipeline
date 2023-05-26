38221ea686a4f68cbdc13b5fa46ffa3ac0b18c29
Andre Kestler <a.kestler@oth-aw.de>
2023-05-26 15:28:19 +0200

    changed mock function , added beforeEach and afterEach, test if method is called

--- sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
+++ sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
@@ -39,6 +39,9 @@ describe("s3Manager - delete Functions", () => {
         deleteImageFromS3Mock.mockImplementation((imageKey) => {
             return new Promise((resolve, reject) => {
                 const deleteImage = {};
+                if(imageKey === "error.jpg") {
+                    reject(new Error("Error"));
+                }
                 resolve(deleteImage);
             })
         });
@@ -47,7 +50,7 @@ describe("s3Manager - delete Functions", () => {
         s3Manager.deleteImageFromS3 = deleteImageFromS3Mock;
 
         // Use mock in test
-        s3Manager.deleteImageFromS3("test_key.jpg")
+        await s3Manager.deleteImageFromS3("test_key.jpg")
             // check if promise is resolved
             .then((result) => {
                 // Check if promise is empty 
@@ -58,4 +61,93 @@ describe("s3Manager - delete Functions", () => {
                 expect(deleteImageFromS3Mock).toHaveBeenCalledWith("test_key.jpg");
             });
     });
+
+    // test if list with all objects is returned
+    test("Check S3Manager.deleteAllImagesFromS3", async () => {
+        // Mock the deleteAllImagesFromS3 function
+        deleteAllImagesFromS3Mock = jest.fn();
+        deleteAllImagesFromS3Mock.mockImplementation(() => {
+            let imageKeys = ["test_key1.jpg", "test_key2.jpg", "test_key3.jpg"];
+            return new Promise((resolve, reject) => {
+                resolve(imageKeys);
+            });
+        });
+
+        let s3Manager = new S3Manager();
+        s3Manager.deleteAllImagesFromS3 = deleteAllImagesFromS3Mock;
+
+        // Use mock in test
+        await s3Manager.deleteAllImagesFromS3()
+            // check if promise is resolved
+            .then((result) => {
+                expect(result).toEqual(["test_key1.jpg", "test_key2.jpg", "test_key3.jpg"]);
+            });
+    });
+});
+
+describe("S3Manager.getImageFromS3() test", () => {
+    // Mock the getImageFromS3 function
+    // S3 returns by success a result object with ConeType and Body
+    // S3 returns by error a error object with code and statusCode
+    getImageFromS3Mock = jest.fn();
+
+    // before each test, mock the function
+    beforeEach(() => {
+        getImageFromS3Mock.mockImplementation((imageKey) => {
+            return new Promise((resolve, reject) => {
+                if (imageKey === "error.jpg") {
+                    const error = {
+                        code: 'NoSuchKey',
+                        statusCode: 404
+                    };
+                    return reject(error);
+                } else {
+                    const result = {
+                        ContentType: 'image/jpeg',
+                        Body: 'Mock Data'
+                    };
+                    return resolve(result);
+                }
+            })
+        });
+    });
+
+    // after each test, reset the mock
+    afterEach(() => {
+        getImageFromS3Mock.mockReset();
+    });
+
+    test("Check if method is called", async () => {
+        let s3Manager = new S3Manager();
+        s3Manager.getImageFromS3 = getImageFromS3Mock;
+
+        await s3Manager.getImageFromS3("test_key.jpg");
+        expect(getImageFromS3Mock).toHaveBeenCalledTimes(1);
+    });
+
+    
+    
+    test("S3Manager.getImageFromS3 - call of the method", async () => {
+
+        let s3Manager = new S3Manager();
+        s3Manager.getImageFromS3 = getImageFromS3Mock;
+
+        await s3Manager.getImageFromS3("test_key.jpg")
+            .then((result) => {
+                console.log(result);
+            });
+        await s3Manager.getImageFromS3("error.jpg")
+            .catch((result) => {
+                console.log(result);
+            });
+
+        
+
+    });
+
+    //test("S3Manager.getImageFromS3 - get image from S3", async () => { });
+
+    //test("S3Manager.getImageFromS3 - get image from S3 failed (wrong key)", async () => {});
+
+    //test("S3Manager.pushImageToS3", async () => {});
 });
