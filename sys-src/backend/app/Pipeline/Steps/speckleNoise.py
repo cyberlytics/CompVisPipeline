e29e90ba3e7f7176b4404d33a49fb73262282777
Antonio Vidos <a.vidos@oth-aw.de>
2023-05-31 21:56:22 +0200

    Write tests for speckle noise step.
    Former-commit-id: 3ead5a125d5c75bc34e3170444a4a2cfc36d2e5d

--- sys-src/backend/app/Pipeline/Steps/speckleNoise.py
+++ sys-src/backend/app/Pipeline/Steps/speckleNoise.py
@@ -8,18 +8,20 @@ class SpeckleNoise(BaseStep):
         #TODO: add error handling
         try:
             p0 = float(parameters[0])
-            gauss = np.random.normal(0, p0, img.shape)
 
             if p0 < 0: p0 = 1
 
+            gauss = np.random.normal(0, p0, img.shape)
+
             if len(img.shape) > 2:
-                gauss = gauss.reshape(img.shape[0], img.shape[1], img.shape(2)).astype("uint8")
+                gauss = gauss.reshape(img.shape[0], img.shape[1], img.shape[2]).astype("uint8")
             else:
                 gauss = gauss.reshape(img.shape[0], img.shape[1]).astype("uint8")
 
-            return cv2.add(img, img*gauss)
+            return cv2.add(img.astype("uint8"), img.astype("uint8")*gauss)
         except Exception:
             raise ImageProcessingError(message="SpeckleNoise failed to process image")
+
 
     def describe(self):
         return {
