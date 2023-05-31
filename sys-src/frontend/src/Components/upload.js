f5539e809edf260bf6b4a2be1dcf57df82e4302c
Andre Kestler <a.kestler@oth-aw.de>
2023-05-31 10:18:54 +0200

    added Button in Upload
    Former-commit-id: 28622773ff24244922ee7218117ab00b2494be67

--- sys-src/frontend/src/Components/upload.js
+++ sys-src/frontend/src/Components/upload.js
@@ -4,14 +4,23 @@ import React from 'react';
 //import Typography from '@mui/material/Typography';
 
 import { Card, CardContent, Typography, Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
-export default function Upload(originalImageID) {
+export default function Upload({originalImageID, setOriginalImageID}) {
+
+    setOriginalImageID = (event) => {
+        console.log("Image Upload");
+    }
 
     return (
         <Card style={{ height: 90 }}>
             <CardContent>
                 <Typography sx={{ width: '100%' }} align="center" variant="h5" component="div">Upload</Typography>
-                
+                <Button variant="contained" color="primary">
+                    {/* <AddIcon />  */}
+                    Upload Image
+                    <input type="file" accept=".jpg" onChange={setOriginalImageID} hidden />
+                </Button>
             </CardContent>
         </Card>
     );
