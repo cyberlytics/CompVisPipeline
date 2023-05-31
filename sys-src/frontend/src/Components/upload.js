41fca758ad56d6f9475b5c90bb35e96f7c10367b
Andre Kestler <a.kestler@oth-aw.de>
2023-05-31 17:32:56 +0200

    centered button
    Former-commit-id: ef47196227a11c2aa9e3c5b76f7a67fac9a4e1cf

--- sys-src/frontend/src/Components/upload.js
+++ sys-src/frontend/src/Components/upload.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Button } from '@mui/material';
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 
 import { v4 as uuidv4 } from 'uuid';
 import S3Manager from './Connections/awsS3';
@@ -30,12 +30,14 @@ export default function Upload({setOriginalImageID}) {
             <CardContent>
                 <Typography sx={{ width: '100%' }} align="center" variant="h5" component="div">Upload</Typography>
                 
-                <label htmlFor="upload-image">
-                    <input id="upload-image" type="file" accept=".jpg" onChange={handleUpload} style={{ display: "none" }}/> 
-                    <Button variant="contained" color="primary" component="span" startIcon={<FileUploadOutlinedIcon />}>
-                        Upload Image
-                    </Button>
-                </label>
+                <div style={{ display: 'flex', justifyContent: 'center'}}>
+                    <label htmlFor="upload-image">
+                        <input id="upload-image" type="file" accept=".jpg" onChange={handleUpload} style={{ display: "none" }}/> 
+                        <Button variant="contained" color="primary" component="span" startIcon={<AddPhotoAlternateIcon />}>
+                            Upload Image
+                        </Button>
+                    </label>
+                </div>
             </CardContent>
         </Card>
     );
