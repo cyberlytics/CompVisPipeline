d96c0c691b77ba2fa763f41ab3f7b7ae81d8f520
Andre Kestler <a.kestler@oth-aw.de>
2023-06-01 14:39:29 +0200

    Image View test - done
    Former-commit-id: 308a2abf1c8bd636d15aa180a5d175bc6d901dac

--- sys-src/frontend/src/Components/imageView.js
+++ sys-src/frontend/src/Components/imageView.js
@@ -4,6 +4,8 @@ import { Card, CardContent, Box } from '@mui/material';
 import S3Manager from './Connections/awsS3';
 
 export default function ImageView({ currentImageID }) {
+    // imageKey is the key to check if image has changed -> just load image from S3 Bucket if a new currentImageID is passed in
+    // imageURL is the URL to the image to be displayed
     const [imageKey, setImageKey] = useState(null);
     const [imageURL, setImageURL] = useState(null);
 
@@ -14,19 +16,17 @@ export default function ImageView({ currentImageID }) {
         s3Manager.getImageFromS3(currentImageID)
             .then( (res) => {
                 // success handling
-                console.log("Image retrieval successful");
                 setImageURL(URL.createObjectURL(new Blob([res.Body], {type: "image/jpg"})));
             })
             .catch( (err) => {
-                console.log("Image retrieval failed");
-                console.log(err);
+                // error handling
+                //console.log("Image retrieval failed");
+                //console.log(err);
             });
-
     }
 
-
     return (
-        <Card style={{ height: "445px" }} >
+        <Card style={{ height: "445px" }} data-testid='imageview-card' >
             <CardContent style={{ height: "100%" }}>
                 <Box display="flex" justifyContent="center" alignItems="center" height="100%">
                     {imageKey && <img src={imageURL} />}
