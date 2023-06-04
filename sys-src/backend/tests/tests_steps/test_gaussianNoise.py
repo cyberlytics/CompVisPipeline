d19db4b0c012bc107530cb2472ae58bdc94ff290
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-04 22:40:10 +0200

    Modify tests for gaussian noise. Tests if exception is raised.
    Former-commit-id: 40db1f904b495d1eb5811996c7003003ff7164a9

--- sys-src/backend/tests/tests_steps/test_gaussianNoise.py
+++ sys-src/backend/tests/tests_steps/test_gaussianNoise.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from app.Pipeline.Steps.gaussianNoise import GaussianNoise
+from app.Pipeline.Steps.gaussianNoise import GaussianNoise, ImageProcessingError
 
 def test_gaussian_noise_with_expected_result():
     image = np.random.randint(0, 255, (10, 10))
@@ -13,14 +13,13 @@ def test_gaussian_noise_with_expected_result():
     assert (image != result).any()
 
 def test_gaussian_noise_with_invalid_strength():
-    image = np.random.randint(0, 255, (10, 10))
+    with pytest.raises(ImageProcessingError):
+        image = np.random.randint(0, 255, (10, 10))
 
-    gaussian_noise_step = GaussianNoise()
+        gaussian_noise_step = GaussianNoise()
 
-    params = [-11]
-    result = gaussian_noise_step(image, params)
-
-    assert (image != result).any()
+        params = [-11]
+        gaussian_noise_step(image, params)
 
 def test_gaussian_noise_with_zero_strength():
     image = np.random.randint(0, 255, (10, 10))
