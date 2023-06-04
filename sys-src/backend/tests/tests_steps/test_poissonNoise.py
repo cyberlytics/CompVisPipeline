5b4c056ed1763a111de93ba29e4d36d1bdfb7054
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-04 23:00:23 +0200

    Add exceptions in poisson noise step. Modify tests for poisson noise. Tests if exceptions are raised.
    Former-commit-id: 247a9b32092e8901f6af6067bd6be2a9edaee84c

--- sys-src/backend/tests/tests_steps/test_poissonNoise.py
+++ sys-src/backend/tests/tests_steps/test_poissonNoise.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from app.Pipeline.Steps.poissonNoise import PoissonNoise
+from app.Pipeline.Steps.poissonNoise import PoissonNoise, ImageProcessingError
 
 def test_poisson_noise_with_expected_result():
     image = np.random.randint(0, 255, (10, 10))
@@ -11,6 +11,16 @@ def test_poisson_noise_with_expected_result():
     result = poisson_noise_step(image, params)
 
     assert (image != result).any()
+
+def test_poisson_noise_with_invalid_image_shape():
+    with pytest.raises(ImageProcessingError):
+        image = np.random.randint(0, 255, 1)
+
+        poisson_noise_step = PoissonNoise()
+        
+        params = []
+        poisson_noise_step(image, params)
+
 
 def test_poisson_noise_with_rgb_image():
     image = np.random.randint(0, 255, (10, 10, 3))
