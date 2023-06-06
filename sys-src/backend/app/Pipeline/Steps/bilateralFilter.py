7b7abac3fc55eb7e354ddf47fd07e996a55a8944
Tobias Weiss <t.weiss@oth-aw.de>
2023-06-06 13:37:38 +0200

    remove pseudocode for bilateralFilter
    Former-commit-id: 7fa51ba495789bdc6ff1cc7e7e243fc1fad3ad85

--- sys-src/backend/app/Pipeline/Steps/bilateralFilter.py
+++ sys-src/backend/app/Pipeline/Steps/bilateralFilter.py
@@ -7,17 +7,9 @@ from app.exceptions import ImageProcessingError
 class BilateralFilter(BaseStep):
     def __call__(self, img, parameters):
         # TODO: add error handling and check which parameters we need
-        try:
-            p0 = int(parameters[0])
-            ...  # TODO: cast parameters to correct type all parameters is a list of str
-            return cv2.bilateralFilter(img, p0)
-        except Exception:
-            raise ImageProcessingError(
-                message="BilateralFilter failed to process image"
-            )
+        return img
 
     def describe(self):
-        # TODO: describe this
         return {
             "title": "Bilateral Filter",
             "info": "Reduce Noise using a Bilateral Filter",
