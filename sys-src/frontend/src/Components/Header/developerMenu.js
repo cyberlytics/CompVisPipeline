7528cc6356378e1fddc7bcec193cce0c7c1f3a16
Andre Kestler <a.kestler@oth-aw.de>
2023-06-06 07:39:13 +0200

    short break
    Former-commit-id: e42969c1485c08a20818c225051c41935c37e98d

---
+++ sys-src/frontend/src/Components/Header/developerMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, Button, CardContent } from '@mui/material';
+import Devider from '@mui/material/Divider';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+import S3Manager from '../Connections/awsS3';
+
+
+export default function DeveloperMenu({developMode}) {
+return (
+    <Card>
+        <CardContent>
+            
+        </CardContent>
+    </Card>
+);
+}
