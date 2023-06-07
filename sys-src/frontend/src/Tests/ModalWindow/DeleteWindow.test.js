e59298d9f1bf4d6d5513a3fab1bce826a4f89029
Andre Kestler <a.kestler@oth-aw.de>
2023-06-07 08:04:27 +0200

    DeleteWindow.js as Componente done
    Former-commit-id: f8ffbc753e14793e6b1d160665d47c711ec2fb90

---
+++ sys-src/frontend/src/Tests/ModalWindow/DeleteWindow.test.js
@@ -0,0 +1,41 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+
+import DeleteWindow from "../../ModalWindow/DeleteWindow";
+
+describe("DeleteWindow.js tests", () => {
+    test("DeleteWindow renders correctly", () => {
+
+    });
+
+    test("DeleteWindow does not appear if open is false", () => {
+
+    });
+
+    test("DeleteWindow appears if open is true", () => {
+
+    });
+
+    test("DeleteWindow has a title and text", () => {
+
+    });
+
+    test("DeleteWindow has a text", () => {
+
+    });
+
+    test("DeleteWindow has a buttons", () => {
+
+    });
+
+    test("DeleteWindow close with cancel button", () => {
+
+    });
+
+    test("DeleteWindow call handle function and closes window", () => {
+
+    });
+
+});
