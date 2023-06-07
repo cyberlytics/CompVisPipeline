a3a6bc718c1652e5149a92717b3073cf98fe7e80
Tobias Lettner <t.lettner@oth-aw.de>
2023-06-07 11:34:34 +0200

    frontend create controller and call in handle login
    Former-commit-id: 56064e0d561076b64eef02369041350497ca2885

--- sys-src/frontend/src/controller.js
+++ sys-src/frontend/src/controller.js
@@ -6,6 +6,29 @@ class Controller {
             .then(response => response.json())
             .then(response => set(response))
     }
+
+    //Call to login in into develop mode
+    static async login(username, password) {
+        const loginCredentials = {
+          username: username,
+          password: password
+        };
+    
+        return fetch('http://127.0.0.1:5000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(loginCredentials)
+        })
+        .then(response => {
+          return response.status === 200
+        })
+        .catch(error => {
+          console.log('Login request error:', error);
+          return false;
+        });
+      }
 }
 
 export default Controller;
