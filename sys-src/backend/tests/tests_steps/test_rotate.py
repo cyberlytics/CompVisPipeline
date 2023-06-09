af729d66e4325e23244a93170ef49af8e31d2bc4
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-09 13:39:52 +0200

    Modify rotate step tests.
    Former-commit-id: f94f1294a4a5dfc9345e7efef25ac213d49aaea6

--- sys-src/backend/tests/tests_steps/test_rotate.py
+++ sys-src/backend/tests/tests_steps/test_rotate.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 
 from app.Pipeline.Steps.rotate import Rotate
-from app.exceptions import ImageProcessingError
+from app.exceptions import WrongParameterError
 
 def test_rotate_with_expected_result_90_degrees():
     image = np.array([
@@ -74,7 +74,7 @@ def test_rotate_with_expected_result_270_degrees():
     assert (result == expected_result).all()
 
 def test_rotate_with_invalid_rotation_parameter():
-    with pytest.raises(ImageProcessingError):
+    with pytest.raises(WrongParameterError):
         image = np.array([
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
@@ -89,7 +89,7 @@ def test_rotate_with_invalid_rotation_parameter():
         rotate_step(image, params)
 
 def test_rotate_with_invalid_image_shape():
-    with pytest.raises(ImageProcessingError):
+    with pytest.raises(WrongParameterError):
         image = np.array([0, 0, 1, 0, 0])
 
         rotate_step = Rotate()
@@ -98,7 +98,7 @@ def test_rotate_with_invalid_image_shape():
         rotate_step(image, params)
 
 def test_rotate_with_invalid_parameter_type():
-    with pytest.raises(ImageProcessingError):
+    with pytest.raises(WrongParameterError):
         image = np.array([
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
@@ -111,6 +111,22 @@ def test_rotate_with_invalid_parameter_type():
         params = ["invalid"]
 
         rotate_step(image, params)
+
+def test_rotate_with_expeceted_image_type():
+    image = np.array([
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0]
+        ]).astype("uint8")
+
+    rotate_step = Rotate()
+    params = [0]
+
+    result = rotate_step(image, params)
+
+    assert result.dtype == image.dtype
 
 def test_rotate_with_rgb_image():
     image = np.random.randint(0, 255, (10, 10, 3))
