86d54c40b9ab0a5c075fa670138e7869a474fedf
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-09 12:06:40 +0200

    Modify gaussian noise step tests.
    Former-commit-id: dba1ab3253aaa30e83a35cae60f8ce678879b692

--- sys-src/backend/tests/tests_steps/test_gaussianNoise.py
+++ sys-src/backend/tests/tests_steps/test_gaussianNoise.py
@@ -1,11 +1,11 @@
 import pytest
 import numpy as np
 from app.Pipeline.Steps.gaussianNoise import GaussianNoise
-from app.exceptions import ImageProcessingError
+from app.exceptions import ImageProcessingError, WrongParameterError
 
 
 def test_gaussian_noise_with_expected_result():
-    image = np.random.randint(0, 255, (10, 10))
+    image = np.random.randint(0, 255, (10, 10)).astype("uint8")
 
     gaussian_noise_step = GaussianNoise()
 
@@ -16,7 +16,7 @@ def test_gaussian_noise_with_expected_result():
 
 
 def test_gaussian_noise_with_invalid_strength():
-    with pytest.raises(ImageProcessingError):
+    with pytest.raises(WrongParameterError):
         image = np.random.randint(0, 255, (10, 10))
 
         gaussian_noise_step = GaussianNoise()
@@ -26,7 +26,7 @@ def test_gaussian_noise_with_invalid_strength():
 
 
 def test_gaussian_noise_with_zero_strength():
-    image = np.random.randint(0, 255, (10, 10))
+    image = np.random.randint(0, 255, (10, 10)).astype("uint8")
 
     gaussian_noise_step = GaussianNoise()
 
@@ -37,7 +37,7 @@ def test_gaussian_noise_with_zero_strength():
 
 
 def test_gaussian_noise_with_invalid_image_shape():
-    with pytest.raises(ImageProcessingError):
+    with pytest.raises(WrongParameterError):
         image = np.random.randint(0, 255, 1)
 
         gaussian_noise_step = GaussianNoise()
@@ -45,9 +45,26 @@ def test_gaussian_noise_with_invalid_image_shape():
         params = [1]
         gaussian_noise_step(image, params)
 
+def test_gaussian_noise_with_invalid_parameter_type():
+    with pytest.raises(WrongParameterError):
+        image = image = np.random.randint(0, 255, (10, 10))
+
+        gaussian_noise_step = GaussianNoise()
+
+        params = ["invalid"]
+        gaussian_noise_step(image, params)
+
+def test_gaussian_noise_with_invalid_image_type():
+    with pytest.raises(ImageProcessingError):
+        image = np.random.randint(0, 255, (10, 10)).astype(np.float32)
+
+        gaussian_noise_step = GaussianNoise()
+
+        params = [1]
+        gaussian_noise_step(image, params)
 
 def test_gaussian_noise_with_rgb_image():
-    image = np.random.randint(0, 255, (10, 10, 3))
+    image = np.random.randint(0, 255, (10, 10, 3)).astype("uint8")
 
     gaussian_noise_step = GaussianNoise()
 
