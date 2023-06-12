8a9507337ab903017534cfc1c636206c87c720be
Marcus Haberl <m.haberl@oth-aw.de>
2023-06-12 16:06:16 +0200

    Button is now not clickable if no image is uploaded. Added two tests
    Former-commit-id: 21457ddced96ba6b4a8ff00278993a1d228d34cf

--- sys-src/frontend/src/Tests/Components/startPipeline.test.js
+++ sys-src/frontend/src/Tests/Components/startPipeline.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+//import React from "react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import StartPipeline from "../../Components/startPipeline";
 
@@ -11,4 +11,28 @@ describe("startPipeline.js tests", () => {
     expect(cardVisible).toBeVisible();
   });
 
+    test('button is disabled when originalImageID is null', () => {
+      const props = {
+        originalImageID: null,
+      };
+
+      const { getByText } = render(<StartPipeline {...props} />);
+
+      const button = getByText('Start Pipeline');
+
+      expect(button).toBeDisabled();
+
+    });
+
+  test('button is clickable when originalImageID is not null', () => {
+    const props = {
+      originalImageID: 'example-image-id',
+    };
+
+    const { getByText } = render(<StartPipeline {...props} />);
+
+    const button = getByText('Start Pipeline');
+
+    expect(button).not.toBeDisabled();
+  });
 });
