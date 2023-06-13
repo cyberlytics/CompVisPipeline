1135c948092b8da7c4895a7f0f6929786292a17f
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-13 14:44:52 +0200

    Implement threshold step.
    Former-commit-id: 05d18915e07b7b4cbf1be0f081773cbbbc3d46db

--- sys-src/backend/app/Pipeline/Steps/threshold.py
+++ sys-src/backend/app/Pipeline/Steps/threshold.py
@@ -1,10 +1,33 @@
+import cv2
+
 from app.Pipeline.Steps.baseStep import BaseStep
+from app.exceptions import ImageProcessingError, WrongParameterError
 
 
 class Thershold(BaseStep):
     def __call__(self, img, parameters):
-        # TODO: implement threshold
-        return img
+        try:
+            p0 = int(parameters[0])
+            p1 = eval(parameters[1])
+
+            if len(img.shape) not in (2, 3): raise WrongParameterError("Invalid image shape!")
+            if p0 < 0: raise WrongParameterError(message="Threshold value must be positive!")
+            if type(p1) is not bool: raise WrongParameterError(message="Invert flag must be a bool!")
+
+            if p1:
+                invert = cv2.THRESH_BINARY_INV
+            else:
+                invert = cv2.THRESH_BINARY
+
+            return cv2.threshold(img, p0, 255, invert)        
+        except WrongParameterError as e:
+            raise e
+        except ValueError as e:
+            raise WrongParameterError(message=e)
+        except NameError as e:
+            raise WrongParameterError(message=e)
+        except Exception as e:
+            raise ImageProcessingError(message=e)
 
     def describe(self):
         return {
