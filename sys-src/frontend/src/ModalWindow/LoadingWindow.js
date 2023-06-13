ff96dfd09a0f3f7329c8f86d1f184bfc7be024f5
Tobias Lettner <t.lettner@oth-aw.de>
2023-06-13 23:20:15 +0200

    add change color if cat hit border
    Former-commit-id: f4d44e709cb0677156f698ce8fe0fda8349f978f

--- sys-src/frontend/src/ModalWindow/LoadingWindow.js
+++ sys-src/frontend/src/ModalWindow/LoadingWindow.js
@@ -14,19 +14,32 @@ export default function LoadingWindow({ open, onClose }) {
 
   const [animationSettings, setAnimationSettings] = useState(
     {
-      positionX: BOUNCINGWIDTH/2,
-      positionY: BOUNCINGHEIGHT/2,
+      positionX: BOUNCINGWIDTH / 2,
+      positionY: BOUNCINGHEIGHT / 2,
       velocityX: 4,
-      velocityY: 4
+      velocityY: 4,
+      backgroundcolor: "transparent"
     })
+
+
+  const generateRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
   const updateAnimationSettings = useCallback(() => {
     setAnimationSettings(prevAnimationSettings => {
       let newPosX = animationSettings.positionX + animationSettings.velocityX;
       let newVelX = newPosX + IMAGESIZE >= BOUNCINGWIDTH - animationSettings.velocityX || newPosX <= 0 - animationSettings.velocityX ? animationSettings.velocityX * -1 : animationSettings.velocityX
-  
+
       let newPosY = animationSettings.positionY + animationSettings.velocityY;
       let newVelY = newPosY + IMAGESIZE >= BOUNCINGHEIGHT - animationSettings.velocityY || newPosY <= 0 - animationSettings.velocityY ? animationSettings.velocityY * -1 : animationSettings.velocityY
+
+      let newColor = (newVelX !== animationSettings.velocityX) || (newVelY !== animationSettings.velocityY) ? generateRandomColor() : animationSettings.backgroundcolor
 
       return {
         ...prevAnimationSettings,
@@ -34,11 +47,12 @@ export default function LoadingWindow({ open, onClose }) {
         positionY: newPosY,
         velocityX: newVelX,
         velocityY: newVelY,
+        backgroundcolor: newColor
       };
     });
-  }, [animationSettings.positionX, animationSettings.positionY, animationSettings.velocityX, animationSettings.velocityY]);
+  }, [animationSettings.positionX, animationSettings.positionY, animationSettings.velocityX, animationSettings.velocityY, animationSettings.backgroundcolor]);
 
-  useEffect(() => {  
+  useEffect(() => {
     if (open === true) {
       const intervalId = setInterval(updateAnimationSettings, 10);
       return () => clearInterval(intervalId);
@@ -54,7 +68,7 @@ export default function LoadingWindow({ open, onClose }) {
         </IconButton>
       </DialogTitle>
       <DialogContent dividers sx={{ width: BOUNCINGWIDTH, height: BOUNCINGHEIGHT, padding: 0 }}>
-          <CardMedia src={cat} component="img" alt="Loading" sx={{ width: `${IMAGESIZE}px`, objectFit: "contain", position: "absolute", transform: `translate(${animationSettings.positionX}px, ${animationSettings.positionY}px)` }} />
+        <CardMedia src={cat} component="img" alt="Loading" sx={{ width: `${IMAGESIZE}px`, objectFit: "contain", position: "absolute", transform: `translate(${animationSettings.positionX}px, ${animationSettings.positionY}px)`, backgroundColor: animationSettings.backgroundcolor }} />
       </DialogContent>
     </Dialog>
   );
