a58243fa18835fd763f768aee602e0772d768ac2
Tobias Lettner <t.lettner@oth-aw.de>
2023-06-19 11:16:44 +0200

    add crop functionality
    Former-commit-id: cddec37327410ed333bc015df8556cfe68040cd8

--- sys-src/backend/app/Pipeline/Steps/crop.py
+++ sys-src/backend/app/Pipeline/Steps/crop.py
@@ -1,10 +1,46 @@
+import numpy as np
 from app.Pipeline.Steps.baseStep import BaseStep
+from app.exceptions import ImageProcessingError, WrongParameterError
 
 
 class Crop(BaseStep):
     def __call__(self, img, parameters):
-        # TODO: implement crop
-        return img
+        try:
+            start_x = int(parameters[0])
+            start_y = int(parameters[1])
+            distance_x = int(parameters[2])
+            distance_y = int(parameters[3])
+            inverse = eval(parameters[4])
+
+            end_x = start_x + distance_x
+            end_y = start_y + distance_y
+
+            if len(img.shape) not in (2, 3):
+                raise WrongParameterError(message="[Crop] Invalid image shape!")
+            if start_x < 0 or start_y < 0:
+                raise WrongParameterError(message="[Crop] Start coordinates must be positive!")
+            if distance_x == 0 or distance_y == 0:
+                raise WrongParameterError(message="[Crop] Distance cannot be zero!")
+            if end_x > img.shape[1] or end_y > img.shape[0]:
+                raise WrongParameterError(message="[Crop] Crop area exceeds image boundaries!")
+            if type(inverse) is not bool: 
+                raise WrongParameterError(message="[Crop] Inverse parameter must be a boolean value!")
+
+            if inverse:
+                cropped_img = np.copy(img)
+                cropped_img[start_y:end_y, start_x:end_x] = 0
+            else:
+                cropped_img = img[start_y:end_y, start_x:end_x]
+
+            return cropped_img
+
+        except WrongParameterError as e:
+            raise e
+        except ValueError as e:
+            raise WrongParameterError(message=f"[Crop] {e}")
+        except Exception as e:
+            raise ImageProcessingError(message=f"[Crop] {e}")
+
 
     def describe(self):
         return {
