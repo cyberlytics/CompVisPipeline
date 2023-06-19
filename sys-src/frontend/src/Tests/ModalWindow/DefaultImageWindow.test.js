abeefa4ed5f76a40f31d9d04c73bf6882f995290
Andre Kestler <a.kestler@oth-aw.de>
2023-06-19 14:45:07 +0200

    added test structure for DefaultImageWindow
    Former-commit-id: 0f1379049caf6423ccf34d9851618f2e2acfc9f0

---
+++ sys-src/frontend/src/Tests/ModalWindow/DefaultImageWindow.test.js
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+
+import DeleteWindow from "../../ModalWindow/DeleteWindow";
+
+describe("DeleteWindow.js tests", () => {
+    test("DefaultImageWindow renders correctly if open is true", () => {
+
+    });
+
+    test("DefaultImageWindow does not appear if open is false", () => {
+
+    });
+
+    test("DefaultImageWindow has a title and text", () => {
+
+    });
+
+    test("DefaultImageWindow has default image and ai image buttons", () => {
+
+    });
+
+    test("DefaultImageWindow close with cancel button", () => {
+
+    });
+
+    test("DefaultImageWindow call handler functions", () => {
+        
+    });
+});
