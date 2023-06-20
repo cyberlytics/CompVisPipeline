a996a1b3134b158e557ea7f7bd5b85f6661ec390
Tobias Dobmeier <t.dobmeier@oth-aw.de>
2023-06-20 16:16:11 +0200

    update tests for opening and closing
    Former-commit-id: 98125205b6065b1f4f8e6e96b6d0fa581478cc5d

--- sys-src/backend/tests/tests_steps/test_opening.py
+++ sys-src/backend/tests/tests_steps/test_opening.py
@@ -4,57 +4,67 @@ from app.Pipeline.Steps.opening import Opening
 from app.exceptions import ImageProcessingError, WrongParameterError
 
 
-def test_successfull_execution_one_channel_image():
-    img = np.zeros((10, 10), dtype=np.uint8)
+def test_successfull_execution_one_channel_image(create_grayscale_image):
     parameters = [0, 3, 3, 1]
 
     opening = Opening()
-    result = opening(img, parameters)
+    result = opening(create_grayscale_image, parameters)
 
-    assert result.shape == (10, 10)
+    assert result.shape == create_grayscale_image.shape
 
 
-def test_successfull_execution_three_channel_image():
-    img = np.zeros((10, 10, 3), dtype=np.uint8)
+def test_successfull_execution_three_channel_image(create_rgb_image):
     parameters = [0, 3, 3, 1]
 
     opening = Opening()
-    result = opening(img, parameters)
+    result = opening(create_rgb_image, parameters)
 
-    assert result.shape == (10, 10, 3)
+    assert result.shape == create_rgb_image.shape
+
+def test_successfull_execution_one_channel_image_kernel_one(create_grayscale_image):
+    parameters = [1, 3, 3, 1]
+
+    opening = Opening()
+    result = opening(create_grayscale_image, parameters)
+
+    assert result.shape == create_grayscale_image.shape
+
+def test_successfull_execution_one_channel_image_kernel_two(create_grayscale_image):
+    parameters = [2, 3, 3, 1]
+
+    opening = Opening()
+    result = opening(create_grayscale_image, parameters)
+
+    assert result.shape == create_grayscale_image.shape
 
 
-def test_invalid_kernel_shape_should_raise_wrong_paramter_error():
-    img = np.zeros((10, 10), dtype=np.uint8)
+def test_invalid_kernel_shape_should_raise_wrong_paramter_error(create_grayscale_image):
     parameters = [3, 3, 3, 1]  # Invalid kernel shape (3)
 
     with pytest.raises(WrongParameterError):
         opening = Opening()
-        opening(img, parameters)
+        opening(create_grayscale_image, parameters)
 
-def test_invalid_kernel_width_should_raise_wrong_paramter_error():
-    img = np.zeros((10, 10), dtype=np.uint8)
+def test_invalid_kernel_width_should_raise_wrong_paramter_error(create_grayscale_image):
     parameters = [0, 1, 3, 1]  # Invalid kernel width (1)
 
     with pytest.raises(WrongParameterError):
         opening = Opening()
-        opening(img, parameters)
+        opening(create_grayscale_image, parameters)
 
-def test_invalid_kernel_height_should_raise_wrong_paramter_error():
-    img = np.zeros((10, 10), dtype=np.uint8)
+def test_invalid_kernel_height_should_raise_wrong_paramter_error(create_grayscale_image):
     parameters = [0, 3, 1, 1]  # Invalid kernel height (1)
 
     with pytest.raises(WrongParameterError):
         opening = Opening()
-        opening(img, parameters)
+        opening(create_grayscale_image, parameters)
 
-def test_invalid_iterations_should_raise_wrong_paramter_error():
-    img = np.zeros((10, 10), dtype=np.uint8)
+def test_invalid_iterations_should_raise_wrong_paramter_error(create_grayscale_image):
     parameters = [0, 3, 3, 0]  # Invalid number of iterations (0)
 
     with pytest.raises(WrongParameterError):
         opening = Opening()
-        opening(img, parameters)
+        opening(create_grayscale_image, parameters)
 
 def test_invalid_image_dtype_should_raise_image_processing_error():
     img = np.zeros((10, 10), dtype=np.float16) # Invalid dtype
