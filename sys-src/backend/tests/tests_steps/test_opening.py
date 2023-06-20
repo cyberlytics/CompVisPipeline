459864574b6da41d320da8e4bd9639520621d47d
Tobias Dobmeier <t.dobmeier@oth-aw.de>
2023-06-20 14:03:51 +0200

    test opening
    Former-commit-id: c735c87864813bdc9755e6775d82f4715e7ea548

---
+++ sys-src/backend/tests/tests_steps/test_opening.py
@@ -0,0 +1,66 @@
+import pytest
+import numpy as np
+from app.Pipeline.Steps.opening import Opening
+from app.exceptions import ImageProcessingError, WrongParameterError
+
+
+def test_successfull_execution_one_channel_image():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    parameters = [0, 3, 3, 1]
+
+    opening = Opening()
+    result = opening(img, parameters)
+
+    assert result.shape == (10, 10)
+
+
+def test_successfull_execution_three_channel_image():
+    img = np.zeros((10, 10, 3), dtype=np.uint8)
+    parameters = [0, 3, 3, 1]
+
+    opening = Opening()
+    result = opening(img, parameters)
+
+    assert result.shape == (10, 10, 3)
+
+
+def test_invalid_kernel_shape_should_raise_wrong_paramter_error():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    parameters = [3, 3, 3, 1]  # Invalid kernel shape (3)
+
+    with pytest.raises(WrongParameterError):
+        opening = Opening()
+        opening(img, parameters)
+
+def test_invalid_kernel_width_should_raise_wrong_paramter_error():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    parameters = [0, 1, 3, 1]  # Invalid kernel width (1)
+
+    with pytest.raises(WrongParameterError):
+        opening = Opening()
+        opening(img, parameters)
+
+def test_invalid_kernel_height_should_raise_wrong_paramter_error():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    parameters = [0, 3, 1, 1]  # Invalid kernel height (1)
+
+    with pytest.raises(WrongParameterError):
+        opening = Opening()
+        opening(img, parameters)
+
+def test_invalid_iterations_should_raise_wrong_paramter_error():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    parameters = [0, 3, 3, 0]  # Invalid number of iterations (0)
+
+    with pytest.raises(WrongParameterError):
+        opening = Opening()
+        opening(img, parameters)
+
+def test_invalid_image_dtype_should_raise_image_processing_error():
+    img = np.zeros((10, 10), dtype=np.float16) # Invalid dtype
+    parameters = [0, 3, 3, 1]  
+
+    with pytest.raises(ImageProcessingError):
+        opening = Opening()
+        opening(img, parameters)
+
