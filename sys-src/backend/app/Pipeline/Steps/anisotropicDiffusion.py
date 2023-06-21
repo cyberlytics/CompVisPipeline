ef51692929a70b4e96f2dcc22ba6117e7d9744dc
Antonio Vidos <a.vidos@oth-aw.de>
2023-06-21 14:45:40 +0200

    Implement anisotropic diffusion step.
    Former-commit-id: deab05e18e7e2e9024f1085c28319b15bab3f682

--- sys-src/backend/app/Pipeline/Steps/anisotropicDiffusion.py
+++ sys-src/backend/app/Pipeline/Steps/anisotropicDiffusion.py
@@ -1,10 +1,27 @@
+import cv2
+
 from app.Pipeline.Steps.baseStep import BaseStep
+from app.exceptions import ImageProcessingError, WrongParameterError
 
 
 class AnisotropicDiffusion(BaseStep):
     def __call__(self, img, parameters):
-        # TODO: implement anisotropic diffusion filter
-        return img
+        try:
+            p0 = float(parameters[0])
+            p1 = float(parameters[1])
+            p2 = int(parameters[2])
+
+            if len(img.shape) < 3 and img.shape[2] != 3: raise WrongParameterError(message="[AnisotropicDiffusion] Image must have three color channels!")
+            if p0 < 0: raise WrongParameterError(message="[AnisotropicDiffusion] Alpha value must be larger than zero!")
+            if p2 < 1: raise WrongParameterError(message="[AnisotropicDiffusion] Number of iterations must be larger than zero!")
+
+            return cv2.ximg.proc.anisotropicDiffusion(img, p0, p1, p2)
+        except WrongParameterError as e:
+            raise e
+        except ValueError as e:
+            raise WrongParameterError(message=f"[AnisotropicDiffusion] {e}")
+        except Exception as e:
+            raise ImageProcessingError(message=f"[AnisotropicDiffusion] {e}")
 
     def describe(self):
         return {
@@ -13,7 +30,7 @@ class AnisotropicDiffusion(BaseStep):
             "params": [
                 {
                     "title": "Alpha Value",
-                    "info": "Amount of time to step forward by in each iteration. Must be positive, normally between 0 and 1.",
+                    "info": "Amount of time to step forward by in each iteration. Must be bigger than zero, normally between 0 and 1.",
                     "defaultValue": 0.1,
                     "value": 0.1,
                 },
