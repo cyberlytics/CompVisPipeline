a8ed393ef45135cb05938e1709517447e7063357
Marcus Haberl <m.haberl@oth-aw.de>
2023-06-22 08:33:09 +0200

    sound is getting played
    Former-commit-id: fcaf7274aa3ebcb056200fc8c1ac0caba6f86e9a

--- sys-src/frontend/src/Components/startPipeline.js
+++ sys-src/frontend/src/Components/startPipeline.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Card, CardContent, Button } from '@mui/material';
 import Controller from "../controller";
+import soundFile from '../resources/cat2.wav'; // Passe den Pfad zur MP3-Datei entsprechend an
 
 export default function StartPipeline(props) {
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
@@ -10,9 +11,15 @@ export default function StartPipeline(props) {
     }, [props.originalImageID]);
 
     const handleButtonClick = async () => {
-        props.setIsLoading(true)
-        await Controller.sendPipelineSteps(props)
-        props.setIsLoading(false)
+        props.setIsLoading(true);
+
+        // Spiele den Sound ab
+        const audio = new Audio(soundFile);
+        audio.play();
+
+        await Controller.sendPipelineSteps(props);
+
+        props.setIsLoading(false);
     };
 
     return (
