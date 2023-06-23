f2cabf9b1a8215f9938b9d03aae6a1eb9c2c2416
Tobias Lettner <t.lettner@oth-aw.de>
2023-06-23 20:58:59 +0200

    add controller tests
    Former-commit-id: 29fe04d348798b9e4d8b7f8fe21c0be7e068cfc9

--- sys-src/frontend/src/Tests/controller.test.js
+++ sys-src/frontend/src/Tests/controller.test.js
@@ -81,6 +81,15 @@ describe("controller.js tests", () => {
     expect(result).toBe(false);
   });
 
+  test("login should return false when error", async () => {
+    const mockError = new Error("Request failed");
+    global.fetch.mockRejectedValueOnce(mockError);
+
+    const result = await Controller.login("username", "password");
+
+    expect(result).toBe(false);
+  });
+
   test("getPipelineStepsFromBackend should fetch available steps and call set function", async () => {
     const mockResponse = {
       step1: "Step 1",
@@ -104,6 +113,35 @@ describe("controller.js tests", () => {
     await new Promise(resolve => setTimeout(resolve, 0)); // Wait for the fetch promise to resolve
 
     expect(setMock).toHaveBeenCalledWith(mockResponse);
+  });
+
+  test("getPipelineStepsFromBackend should not set result when fetch is aborted", async () => {
+    const mockResponse = {
+      name: 'AbortError'
+    };
+    global.fetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockResponse),
+    });
+
+    const setMock = jest.fn();
+
+    const originalAbortController = window.AbortController;
+    window.AbortController = jest.fn(() => ({
+      signal: 'AbortSignal',
+      abort: jest.fn(),
+    }));
+
+    global.fetch.mockRejectedValueOnce(mockResponse);
+    await Controller.getPipelineStepsFromBackend(setMock);
+    expect(setMock).not.toHaveBeenCalled();
+    window.AbortController = originalAbortController;
+  });
+
+  test("getPipelineStepsFromBackend should not set result", async () => {
+    const setMock = jest.fn();
+    global.fetch.mockRejectedValueOnce(new Error('Some error occurred.'));
+    await Controller.getPipelineStepsFromBackend(setMock);
+    expect(setMock).not.toHaveBeenCalled();
   });
 
   test("sendPipelineSteps should send pipeline steps and set result on success", async () => {
@@ -168,6 +206,95 @@ describe("controller.js tests", () => {
     expect(toastSuccessSpy).toHaveBeenCalledWith("Pipeline completed successfully.");
   });
 
+  test("sendPipelineSteps should handle error response", async () => {
+    const props = {
+      originalImageID: "image123",
+      steps: [
+        {
+          title: "Bilateral Filter",
+          params: [
+            { defaultValue: 3, info: "Info 1", title: "Title 1", value: 3 },
+            { defaultValue: 0.02, info: "Info 2", title: "Title 2", value: 0.02 },
+          ],
+          info: "Info",
+          id: 0,
+          uuid: "uuid1",
+        },
+        {
+          title: "Gaussian Blur",
+          params: [
+            { defaultValue: 5, info: "Info 3", title: "Title 3", value: 5 },
+            { defaultValue: 0, info: "Info 4", title: "Title 4", value: 0 },
+          ],
+          info: "Info",
+          id: 1,
+          uuid: "uuid2",
+        },
+      ],
+      setPipelineResult: jest.fn(),
+    };
+
+    const mockErrorResponse = {
+      error: "Invalid pipeline steps",
+    };
+
+    global.fetch.mockResolvedValueOnce({
+      ok: false,
+      json: jest.fn().mockResolvedValueOnce(mockErrorResponse),
+    });
+
+    await Controller.sendPipelineSteps(props);
+
+    expect(props.setPipelineResult).toHaveBeenCalledWith([]);
+  });
+
+  test("sendPipelineSteps should handle abort response", async () => {
+    const props = {
+      originalImageID: "image123",
+      steps: [
+        {
+          title: "Bilateral Filter",
+          params: [
+            { defaultValue: 3, info: "Info 1", title: "Title 1", value: 3 },
+            { defaultValue: 0.02, info: "Info 2", title: "Title 2", value: 0.02 },
+          ],
+          info: "Info",
+          id: 0,
+          uuid: "uuid1",
+        },
+        {
+          title: "Gaussian Blur",
+          params: [
+            { defaultValue: 5, info: "Info 3", title: "Title 3", value: 5 },
+            { defaultValue: 0, info: "Info 4", title: "Title 4", value: 0 },
+          ],
+          info: "Info",
+          id: 1,
+          uuid: "uuid2",
+        },
+      ],
+      setPipelineResult: jest.fn(),
+    };
+
+    const originalAbortController = window.AbortController;
+    const abortController = new AbortController();
+    window.AbortController = jest.fn(() => abortController);
+
+    const abortError = new Error("AbortError");
+    abortError.name = "AbortError";
+
+    global.fetch.mockImplementationOnce(() => {
+      abortController.abort();
+      return delay();
+    });
+
+    await Controller.sendPipelineSteps(props);
+
+    expect(props.setPipelineResult).toHaveBeenCalledWith([]);
+
+    window.AbortController = originalAbortController;
+  });
+
   test("getImageMetadataFromBackend should fetch image metadata and call setMetadata function", async () => {
     const imageId = "image123";
     const setMetadataMock = jest.fn();
@@ -194,12 +321,12 @@ describe("controller.js tests", () => {
       })
     );
 
-    await new Promise((resolve) => setTimeout(resolve, 0)); // Warten, bis der Fetch-Aufruf abgeschlossen ist
+    await new Promise((resolve) => setTimeout(resolve, 0));
 
     expect(setMetadataMock).toHaveBeenCalledWith(mockResponse);
     expect(toastErrorSpy).not.toHaveBeenCalled();
   });
-
+  
   test('abortFetch should abort the fetch and create a new AbortController', () => {
     const mockAbortControllerConstructor = jest.fn();
 
@@ -230,18 +357,34 @@ describe("controller.js tests", () => {
     const toastErrorSpy = jest.spyOn(toast, "error");
 
     await Controller.getAiImageFromBackend(setOriginalImageIDMock, setCurrentImageIDMock, setMetadataMock);
-  
+
     expect(global.fetch).toHaveBeenCalledWith(
       "http://127.0.0.1:5000/random-ai-fatcat",
       expect.objectContaining({
         signal: Controller.abortController.signal,
       })
     );
-  
+
     await new Promise((resolve) => setTimeout(resolve, 0)); // Warten, bis der Fetch-Aufruf abgeschlossen ist
-  
+
     expect(setMetadataMock).toHaveBeenCalledWith(mockResponse);
-    expect(toastErrorSpy).not.toHaveBeenCalled();  
+    expect(toastErrorSpy).not.toHaveBeenCalled();
   });
 
+  test("getAiImageFromBackend should handle fetch abort error", async () => {
+    const setOriginalImageIDMock = jest.fn();
+    const setCurrentImageIDMock = jest.fn();
+    const setMetadataMock = jest.fn();
+  
+    global.fetch.mockRejectedValueOnce(new Error('AbortError'));
+  
+    await Controller.getAiImageFromBackend(
+      setOriginalImageIDMock,
+      setCurrentImageIDMock,
+      setMetadataMock
+    );
+  
+    expect(setMetadataMock).not.toHaveBeenCalled();
+  });
+  
 });
