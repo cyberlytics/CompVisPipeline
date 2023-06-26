0b6346ee7f890ad72afdf6ad6c94558b7631eee4
Andre Kestler <a.kestler@oth-aw.de>
2023-06-26 06:44:49 +0200

    add controller to junk with getSessionTokenFromBackend()
    Former-commit-id: ec3a32787f52de6f34e393c3c9aec9936502a6fe

---
+++ sys-src/frontend/junk/controller.js
@@ -0,0 +1,18 @@
+static async getSessionTokenFromBackend() {
+    await fetch(base+'/get_token', {
+      signal: Controller.abortController.signal,
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log("in controller: ", response);
+      return {accessKeyId: response.accessKeyId, secretAccessKey: response.secretAccessKey, region: response.region};
+    })
+    .catch(error => {
+      if (error.name === 'AbortError') {
+        toast.error('Fetch aborted');
+      }
+      else {
+        toast.error('Error retrieving session token:', error);
+      }
+    });
+  }
